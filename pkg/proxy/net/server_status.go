2e20a48c9de0fcde2a0c7d458804c9b9404b0910
djshow832 <873581766@qq.com>
2023-12-15 15:08:47 +0800

    add server_status

---
+++ pkg/proxy/net/server_status.go
@@ -0,0 +1,20 @@
+// Copyright 2023 PingCAP, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package net
+
+// Server information.
+const (
+	ServerStatusInTrans            uint16 = 0x0001
+	ServerStatusAutocommit         uint16 = 0x0002
+	ServerMoreResultsExists        uint16 = 0x0008
+	ServerStatusNoGoodIndexUsed    uint16 = 0x0010
+	ServerStatusNoIndexUsed        uint16 = 0x0020
+	ServerStatusCursorExists       uint16 = 0x0040
+	ServerStatusLastRowSend        uint16 = 0x0080
+	ServerStatusDBDropped          uint16 = 0x0100
+	ServerStatusNoBackslashEscaped uint16 = 0x0200
+	ServerStatusMetadataChanged    uint16 = 0x0400
+	ServerStatusWasSlow            uint16 = 0x0800
+	ServerPSOutParams              uint16 = 0x1000
+)
