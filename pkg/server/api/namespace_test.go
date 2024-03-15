384e746b38775f5a7197afe774f2010a60422011
djshow832 <zhangming@pingcap.com>
2024-03-15 22:38:11 +0800

    api: support get json format for config by setting Accept header (#484)

--- pkg/server/api/namespace_test.go
+++ pkg/server/api/namespace_test.go
@@ -16,7 +16,7 @@ func TestNamespace(t *testing.T) {
 	_, doHTTP := createServer(t, nil)
 
 	// test list
-	doHTTP(t, http.MethodGet, "/api/admin/namespace", nil, func(t *testing.T, r *http.Response) {
+	doHTTP(t, http.MethodGet, "/api/admin/namespace", nil, nil, func(t *testing.T, r *http.Response) {
 		all, err := io.ReadAll(r.Body)
 		require.NoError(t, err)
 		require.Equal(t, `""`, string(all))
@@ -24,15 +24,15 @@ func TestNamespace(t *testing.T) {
 	})
 
 	// test set
-	doHTTP(t, http.MethodPut, "/api/admin/namespace/gg", strings.NewReader(`{}`), func(t *testing.T, r *http.Response) {
+	doHTTP(t, http.MethodPut, "/api/admin/namespace/gg", strings.NewReader(`{}`), nil, func(t *testing.T, r *http.Response) {
 		require.Equal(t, http.StatusOK, r.StatusCode)
 	})
-	doHTTP(t, http.MethodPut, "/api/admin/namespace", strings.NewReader(`{"namespace": "dge"}`), func(t *testing.T, r *http.Response) {
+	doHTTP(t, http.MethodPut, "/api/admin/namespace", strings.NewReader(`{"namespace": "dge"}`), nil, func(t *testing.T, r *http.Response) {
 		require.Equal(t, http.StatusOK, r.StatusCode)
 	})
 
 	// test get
-	doHTTP(t, http.MethodGet, "/api/admin/namespace/dge", nil, func(t *testing.T, r *http.Response) {
+	doHTTP(t, http.MethodGet, "/api/admin/namespace/dge", nil, nil, func(t *testing.T, r *http.Response) {
 		all, err := io.ReadAll(r.Body)
 		require.NoError(t, err)
 		require.Equal(t, `{"namespace":"dge","frontend":{"user":"","security":{}},"backend":{"instances":null,"security":{}}}`, string(all))
@@ -40,18 +40,18 @@ func TestNamespace(t *testing.T) {
 	})
 
 	// test remove
-	doHTTP(t, http.MethodDelete, "/api/admin/namespace/dge", nil, func(t *testing.T, r *http.Response) {
+	doHTTP(t, http.MethodDelete, "/api/admin/namespace/dge", nil, nil, func(t *testing.T, r *http.Response) {
 		require.Equal(t, http.StatusOK, r.StatusCode)
 	})
-	doHTTP(t, http.MethodGet, "/api/admin/namespace/dge", nil, func(t *testing.T, r *http.Response) {
+	doHTTP(t, http.MethodGet, "/api/admin/namespace/dge", nil, nil, func(t *testing.T, r *http.Response) {
 		require.Equal(t, http.StatusInternalServerError, r.StatusCode)
 	})
 
 	// test commit
-	doHTTP(t, http.MethodPost, "/api/admin/namespace/commit?namespace=xx", nil, func(t *testing.T, r *http.Response) {
+	doHTTP(t, http.MethodPost, "/api/admin/namespace/commit?namespace=xx", nil, nil, func(t *testing.T, r *http.Response) {
 		require.Equal(t, http.StatusInternalServerError, r.StatusCode)
 	})
-	doHTTP(t, http.MethodPost, "/api/admin/namespace/commit", nil, func(t *testing.T, r *http.Response) {
+	doHTTP(t, http.MethodPost, "/api/admin/namespace/commit", nil, nil, func(t *testing.T, r *http.Response) {
 		require.Equal(t, http.StatusInternalServerError, r.StatusCode)
 	})
 }
