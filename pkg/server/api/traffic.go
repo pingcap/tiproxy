129ff0aff8d5804207e4783c0c5d15037c3d5b93
djshow832 <zhangming@pingcap.com>
2025-05-23 10:13:05 +0800

    Fix conflict

--- pkg/server/api/traffic.go
+++ pkg/server/api/traffic.go
@@ -37,31 +37,6 @@ func (h *Server) TrafficCapture(c *gin.Context) {
 		}
 		cfg.Duration = duration
 	}
-<<<<<<< HEAD
-=======
-	cfg.EncryptMethod = c.PostForm("encrypt-method")
-
-	compress := true
-	if compressStr := c.PostForm("compress"); compressStr != "" {
-		var err error
-		if compress, err = strconv.ParseBool(compressStr); err != nil {
-			h.lg.Warn("parsing argument 'compress' error, using true", zap.String("compress", c.PostForm("compress")), zap.Error(err))
-			compress = true
-		}
-	}
-	cfg.Compress = compress
-	cfg.KeyFile = globalCfg.Security.Encryption.KeyPath
-	if startTimeStr := c.PostForm("start-time"); startTimeStr != "" {
-		startTime, err := time.Parse(time.RFC3339, startTimeStr)
-		if err != nil {
-			c.String(http.StatusBadRequest, err.Error())
-			return
-		}
-		cfg.StartTime = startTime
-	} else {
-		cfg.StartTime = time.Now()
-	}
->>>>>>> acbb0eb (config, api: add a config to turn off traffic replay on TiDB Cloud (#738))
 
 	if err := h.mgr.ReplayJobMgr.StartCapture(cfg); err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
@@ -89,11 +64,6 @@ func (h *Server) TrafficReplay(c *gin.Context) {
 	}
 	cfg.Username = c.PostForm("username")
 	cfg.Password = c.PostForm("password")
-<<<<<<< HEAD
-=======
-	cfg.ReadOnly = strings.EqualFold(c.PostForm("readonly"), "true")
-	cfg.KeyFile = globalCfg.Security.Encryption.KeyPath
->>>>>>> acbb0eb (config, api: add a config to turn off traffic replay on TiDB Cloud (#738))
 
 	if err := h.mgr.ReplayJobMgr.StartReplay(cfg); err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
