3a6db40bcaebfa0634bed60acc3d820a55b476a0
djshow832 <zhangming@pingcap.com>
2025-09-10 19:10:26 +0800

    add rotate

--- pkg/sqlreplay/store/const.go
+++ pkg/sqlreplay/store/const.go
@@ -6,10 +6,12 @@ package store
 import "time"
 
 const (
-	fileNamePrefix     = "traffic-"
-	fileNameSuffix     = ".log"
-	fileCompressFormat = ".gz"
-	fileSize           = 300 << 20
-	bufferSize         = 1 << 20
-	opTimeout          = 10 * time.Second
+	fileNamePrefix      = "traffic-"
+	auditFileNamePrefix = "tidb-audit-"
+	fileNameSuffix      = ".log"
+	logTimeLayout       = "2006-01-02T15-04-05.999"
+	fileCompressFormat  = ".gz"
+	fileSize            = 300 << 20
+	bufferSize          = 1 << 20
+	opTimeout           = 10 * time.Second
 )
