4665c9f48e086d16a7b6d63c4e6133b813eff327
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-12-18 19:33:07 +0800

    proxy, metrics: add metrics on QPS distribution and connection lifetime (#1040) (#1043)
    Co-authored-by: djshow832 <zhangming@pingcap.com>

--- pkg/metrics/session.go
+++ pkg/metrics/session.go
@@ -39,4 +39,22 @@ var (
 			Help:      "Bucketed histogram of processing time (s) of handshakes.",
 			Buckets:   prometheus.ExponentialBuckets(0.0005, 2, 29), // 0.5ms ~ 1.5days
 		}, []string{LblBackend})
+
+	QueryTimeSinceConnCreationHistogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: ModuleProxy,
+			Subsystem: LabelSession,
+			Name:      "query_time_since_conn_creation_seconds",
+			Help:      "Bucketed histogram of query start time (s) since connection creation.",
+			Buckets:   prometheus.ExponentialBuckets(1, 2, 21), // 1s ~ 24days
+		})
+
+	ConnLifetimeHistogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: ModuleProxy,
+			Subsystem: LabelSession,
+			Name:      "conn_lifetime_seconds",
+			Help:      "Bucketed histogram of connection lifetime (s).",
+			Buckets:   prometheus.ExponentialBuckets(0.1, 2, 25), // 1s ~ 38days
+		})
 )
