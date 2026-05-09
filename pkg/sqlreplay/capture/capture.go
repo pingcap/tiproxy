8b4a0e134c97d296c152bee4c2c6e0dcd89d2784
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2026-05-09 17:18:44 +0800

    *: increase the server error counter for more errors (#842) (#1147)
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    Co-authored-by: djshow832 <zhangming@pingcap.com>

--- pkg/sqlreplay/capture/capture.go
+++ pkg/sqlreplay/capture/capture.go
@@ -11,10 +11,10 @@ import (
 	"time"
 
 	"github.com/pingcap/tiproxy/lib/util/errors"
-	"github.com/pingcap/tiproxy/lib/util/waitgroup"
 	pnet "github.com/pingcap/tiproxy/pkg/proxy/net"
 	"github.com/pingcap/tiproxy/pkg/sqlreplay/cmd"
 	"github.com/pingcap/tiproxy/pkg/sqlreplay/store"
+	"github.com/pingcap/tiproxy/pkg/util/waitgroup"
 	"github.com/siddontang/go/hack"
 	"go.uber.org/zap"
 )
